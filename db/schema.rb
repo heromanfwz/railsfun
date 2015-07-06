6a32c12c094b9eea2cad4bf5f7b16d91e2a3610b
Neember <heroman@futureworkz.com>
2015-07-06 16:00:58 +0700

    change sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706075314) do
+ActiveRecord::Schema.define(version: 20150706081827) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "catelogies", force: :cascade do |t|
     t.string   "title"
@@ -24,11 +27,11 @@ ActiveRecord::Schema.define(version: 20150706075314) do
     t.text     "desription"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "price",       precision: 6, scale: 2
+    t.decimal  "price",       precision: 10, scale: 2
     t.boolean  "published"
     t.integer  "category_id"
   end
 
-  add_index "products", ["category_id"], name: "index_products_on_category_id"
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
 end
