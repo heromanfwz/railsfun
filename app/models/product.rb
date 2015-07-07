11a6aec98dae49cbe20e6c4b704e4d07f99f3aaf
Neember <heroman@futureworkz.com>
2015-07-07 10:19:06 +0700

    strip html from description on models

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,13 @@ class Product < ActiveRecord::Base
   scope	:priced_more_than,		->(price) { where('price > ?', price) }
   scope	:description_includes, 	->(description) { where('description ILIKE ?', "%#{description}%") }
 
+  before_save	:strip_html_from_description
+
+  def strip_html_from_description
+  	self.description =
+  			ActionView::Base.full_sanitizer.sanitize(self.description)	
+  end
+
   def title_is_shorter_than_description
   	return if title.blank? or description.blank?
   	if description.length < title.length
