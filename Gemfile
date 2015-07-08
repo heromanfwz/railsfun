1d1dc165457493ecf2ada1d7ca7c482c406809f0
Neember <heroman@futureworkz.com>
2015-07-08 09:51:41 +0700

    strong parameter

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'enumerize'
 
 gem 'countries'
+
+gem 'formtastic', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -44,5 +46,7 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
 	end
+
+	def new
+		@product = Product.new
+	end
+
+
+	def create
+		product_params = params.require(:product).permit( :title,
+														  :description,
+														  :price,
+														  :published,
+														  :category_id )
+		@product = Product.new(product_params)
+		@product.save
+		redirect_to products_url
+	end
 end
