9a82535960ba21b17b81cdf2e11fc9935839770c
Neember <heroman@futureworkz.com>
2015-07-08 08:38:02 +0700

    show products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all.includes(:category).published
 	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
 end
